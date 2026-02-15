2d186181da03ef94f34eab475afa58c50cdf0457
Daffarez Elguska <daffaelguska@gmail.com>
2026-02-15 21:07:38 +0700

    separate function to make the code cleaner

---
+++ qr_service.py
@@ -0,0 +1,20 @@
+import qrcode
+
+
+def generate_qr_image(data: str):
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=8,
+        border=4,
+    )
+
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    return img
+
+
+def save_image(img, path: str):
+    img.save(path)
